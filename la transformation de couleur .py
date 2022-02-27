89db3901e160f0599c93134618ff3f2bcc84245e
CHASSARD <100364097+Wellan76@users.noreply.github.com>
2022-02-27 15:39:24 +0100

    Add files via upload

---
+++ la transformation de couleur .py
@@ -0,0 +1,42 @@
+﻿import matplotlib.image as mpimag
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+
+imageA=mpimag.imread('Marcel Sembat.png')
+plt.imshow(imageA, interpolation='nearest')
+plt.axis('off')
+plt.show()
+
+imagemichelle=np.zeros((587,1020,3))
+for y in range(1020):
+    for x in range(587):
+        imagemichelle[x,y]=imageA[x,y]
+print(imagemichelle)
+
+
+
+img=Image.open('Marcel Sembat.png')
+largeur_image=10
+hauteur_image=5
+imageb=np.zeros((587,1020,3))
+for y in range(1020):
+    for x in range(587):
+        """r,v,b=img.getpixel((x,y))"""
+
+
+        Y  = imagemichelle[x,y,0] *  0.29900 + imagemichelle[x,y,1] *  0.58700 + imagemichelle[x,y,2] *  0.11400
+        Cb = imagemichelle[x,y,0] * -0.16874 + imagemichelle[x,y,1] * -0.33126 + imagemichelle[x,y,2] *  0.50000 + 128
+        Cr = imagemichelle[x,y,0] *  0.50000 + imagemichelle[x,y,1] * -0.41869 + imagemichelle[x,y,2] * -0.08131 + 128
+
+        imageb[x,y,0]=Y
+        imageb[x,y,1]=Cb
+        imageb[x,y,2]=Cr
+
+
+plt.imshow(imageb, interpolation='nearest')
+plt.axis('off')
+plt.show()
+
+print("fin")
+
---
+++ le découpage en bloc de pixel.py
@@ -0,0 +1,40 @@
+﻿import matplotlib.image as mpimag
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+from scipy import misc
+
+M =mpimag.imread('Marcel Sembat.png')
+imageA=mpimag.imread('Marcel Sembat.png')
+plt.imshow(imageA, interpolation='nearest')
+plt.axis('off')
+plt.show()
+taille =M.shape
+P=taille[0]
+M=taille[1]
+
+imageB=np.zeros((16,16,3))
+for y in range(0,16):
+    for x in range(0,16):
+        imageB[x,y]=imageA[x,y]
+print(imageB)
+
+imageC=np.zeros((16,16,3))
+for y in range(0,16):
+    for x in range(16,32):
+        imageC[x-16,y]=imageA[x,y]
+print(imageC)
+
+
+plt.imshow(imageB, interpolation='nearest')
+plt.axis('off')
+plt.show()
+plt.imshow(imageC, interpolation='nearest')
+plt.axis('off')
+plt.show()
+
+
+print(taille)
+print(P)
+print(M)
+print("fin")
